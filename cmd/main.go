81b15cbc6a94363011af66d94bb9cf109eda78f4
jedrom <romankiewicz.j@gmail.com>
2023-05-09 15:20:54 +0200

    working example

--- cmd/main.go
+++ cmd/main.go
@@ -5,9 +5,20 @@ import (
 )
 
 func main() {
+
 	ui := gui.NewGUI(true)
 
-	board := gui.NewBoard(1, 1, nil)
-	ui.Draw(board)
+	board1 := gui.NewBoard(1, 1, nil)
+	board2 := gui.NewBoard(50, 1, nil)
+	ui.Draw(board1)
+	ui.Draw(board2)
+
+	states := [10][10]gui.State{}
+	for i := range states {
+		states[i] = [10]gui.State{}
+	}
+	board1.SetStates(states)
+
 	ui.Start(nil)
 }
+
